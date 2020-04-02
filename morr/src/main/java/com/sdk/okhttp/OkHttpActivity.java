b33d35ef435c68f1c7e28bd6b3fe590e7f2cebd2
llb <13917944029@163.com>
2020-04-02 18:25:23 +0800

    okhttp

---
+++ morr/src/main/java/com/sdk/okhttp/OkHttpActivity.java
@@ -0,0 +1,148 @@
+package com.sdk.okhttp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sdk.morr.R;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Headers;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okio.BufferedSink;
+
+public class OkHttpActivity extends AppCompatActivity {
+
+    private static final String TAG = "OkHttpActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_okhttp);
+
+    }
+
+    /**
+     * get请求
+     * @param view
+     */
+    public void doGet(View view) {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://www.baidu.com")
+                .get()
+                .build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.d(TAG,e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                Log.d(TAG, "onResponse: " + response.body().string());
+            }
+        });
+
+    }
+
+
+    /**
+     * post请求
+     *
+     * @param view
+     */
+    public void doPost(View view) {
+
+//        RequestBody requestBody = new RequestBody() {
+//            @org.jetbrains.annotations.Nullable
+//            @Override
+//            public MediaType contentType() {
+//                return MediaType.parse("text/x-markdown; charset=utf-8");
+//            }
+//
+//            @Override
+//            public void writeTo(@NotNull BufferedSink bufferedSink) throws IOException {
+//                bufferedSink.writeUtf8("I am Jdqm.");
+//            }
+//        };
+
+
+        MediaType mediaType = MediaType.parse("text/x-markdown; charset=utf-8");
+        String requestBody = "I am hjy";
+        Request request = new Request.Builder()
+                .url("https://api.github.com/markdown/raw")
+                .post(RequestBody.create(mediaType,requestBody))
+                .build();
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                Log.d(TAG, response.protocol() + " " + response.code() + " " + response.message());
+                Headers headers = response.headers();
+                for (int i = 0; i < headers.size(); i++) {
+                    Log.d(TAG, headers.name(i) + ":" + headers.value(i));
+                }
+                Log.d(TAG, "onResponse: " + response.body().string());
+            }
+        });
+
+    }
+
+    /**
+     * post提交表单
+     * @param view
+     */
+    public void doPostForm(View view) {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+
+        RequestBody requestBody = new FormBody.Builder().add("search","Jurassic Park").build();
+
+        Request request = new Request.Builder()
+                .url("https://en.wikipedia.org/w/index.php")
+                .post(requestBody)
+                .build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.d(TAG, "onFailure: " + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                Log.d(TAG, response.protocol() + " " +response.code() + " " + response.message());
+                Headers headers = response.headers();
+                for (int i = 0; i < headers.size(); i++) {
+                    Log.d(TAG, headers.name(i) + ":" + headers.value(i));
+                }
+                Log.d(TAG, "onResponse: " + response.body().string());
+            }
+        });
+
+    }
+}
