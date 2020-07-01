a0d212e21b779128e3f618895ef63b1488051817
llb <13917944029@163.com>
2020-07-01 15:52:16 +0800

    44

--- app/src/main/java/com/sdk/studydemo/shuju/arry/ArrayList.java
+++ app/src/main/java/com/sdk/studydemo/shuju/arry/ArrayList.java
@@ -1,18 +1,129 @@
 package com.sdk.studydemo.shuju.arry;
 
-public class ArrayList {
+import androidx.annotation.NonNull;
+
+public class ArrayList<E> {
     private int size;
-    private int[] elements;
+    private E[] elements;
 
-    private static final int DEFAULT_CAPTICY = 10;
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final int ELEMENT_NOT_FOUND = -1;
 
-    public ArrayList(int capaticy) {
-        capaticy = (capaticy < DEFAULT_CAPTICY) ? DEFAULT_CAPTICY : capaticy;
-        elements = new int[capaticy];
+    public ArrayList(int capacity) {
+        capacity = (capacity < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capacity;
+        elements = (E[]) new Object[capacity];
     }
 
     public ArrayList() {
-        this(DEFAULT_CAPTICY);
+        this(DEFAULT_CAPACITY);
     }
 
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public E get(int index) {
+        rangeCheck(index);
+        return elements[index];
+    }
+
+
+    public E set(int index, E element) {
+        rangeCheck(index);
+        E old = elements[index];
+        elements[index] = element;
+        return old;
+    }
+
+    public int indexOf(E element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i] == element) return i;
+        }
+        return ELEMENT_NOT_FOUND;
+    }
+
+    public boolean contains(E element) {
+        return indexOf(element) != ELEMENT_NOT_FOUND;
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    public void add(E element) {
+        //elements[size++] = element;
+        add(size, element);
+    }
+
+    public void add(int index, E element) {
+
+        rangeCheckForAdd(index);
+
+        ensureCapacity(size + 1);
+
+        for (int i = size - 1; i >= index; i--) {
+            elements[i + 1] = elements[i];
+        }
+        elements[index] = element;
+        size++;
+    }
+
+    private void ensureCapacity(int capacity) {
+        int oldCapacity = elements.length;
+        if (oldCapacity >= capacity) return;
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        E[] newElements = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newElements[i] = elements[i];
+        }
+        elements = newElements;
+        System.out.println(oldCapacity + "扩容为" + newCapacity);
+    }
+
+    public E remove(int index) {
+
+        rangeCheck(index);
+
+        E old = elements[index];
+        for (int i = index + 1; i <= size - 1; i++) {
+            elements[i - 1] = elements[i];
+        }
+        size--;
+        elements[size] = null;
+        return old;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(size).append(" [");
+        for (int i = 0; i < size; i++) {
+            if (i != 0) {
+                sb.append(", ");
+            }
+            sb.append(elements[i]);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    private void rangeCheck(int index) {
+        if (index < 0 || index >= size) {
+            outOfBounds(index);
+        }
+    }
+
+    private void rangeCheckForAdd(int index) {
+        if (index < 0 || index > size) {
+            outOfBounds(index);
+        }
+    }
+
+    private void outOfBounds(int index) {
+        throw new IndexOutOfBoundsException("Index:" + index + ",Size:" + size);
+    }
 }
